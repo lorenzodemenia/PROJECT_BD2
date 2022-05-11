6fd7ff8e908dc9b41a77992731d12319b69df086
radu <obs@live.it>
2022-05-12 00:20:07 +0200

    Alcuni cambi, aggiunto il file search.py e fatto un template per la navbar

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from struttura_db import *
 from auth import *
+from search import *
 
 if __name__ == '__main__':
     app.run()
---
+++ search.py
@@ -0,0 +1,13 @@
+from struttura_db import *
+
+
+@app.route('/search', methods=['POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        print('yuppi')
+
+        search = request.form['search']
+        print(search)
+
+    return render_template('Home/search.html', search=search)
