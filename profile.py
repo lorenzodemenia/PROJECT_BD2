dee6377317e804af32cd3f2dad6f57a7cb392ef8
DanielePavan <danipav95@gmail.com>
2022-07-16 14:27:02 +0200

    aggiornamento

--- profile.py
+++ profile.py
@@ -45,6 +45,13 @@ def stats_album():
     return album
 
 
+
+
+def upload_user_image():
+    return os.path.join(app.config['UPLOAD_FOLDER'], current_user.image)
+
+''' , user_image=upload_user_image() '''
+
 @app.route('/artist_page',  methods=['GET', 'POST'])
 def artist_page():
 
